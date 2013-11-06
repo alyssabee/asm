3e5be09037a78bf009e2e514d1488e913a34cb9c
puppy <puppy@puppy-PC.(none)>
2013-11-06 00:52:49 -0500

    cleaning

--- test.s
+++ test.s
@@ -6,9 +6,15 @@
 
 _start:
 
-	mov	$0x20, %eax
-	
+	mov	$0x4, %eax
+	mov	$1, %ebx
+	mov	$message, %ecx
+	mov	$10, %edx
+	int	$0x80
+
 	mov	$1, %eax
 	int	$0x80
 
+message:
+	.ascii "Test call\n"
 .end
